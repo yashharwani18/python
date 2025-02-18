8cb835bfc40b4eaf367015a917b25a92522960c5
Yash <126963834+yashharwani18@users.noreply.github.com>
2025-02-18 15:11:49 +0530

    Add files via upload

--- week6/week6-1.py
+++ week6/week6-1.py
@@ -1,8 +1,8 @@
 def count(names):
-    bcount = sum(1 for name in names if isinstance(name,tuple))
+    bcount = sum(len(name) for name in names if isinstance(name,tuple))
     gcount = sum(1 for name in names if isinstance(name,str))
     print(f"boys: {bcount}")
     print(f"girls: {gcount}")
     
-names = [("Aarav",), "Anannya", ("Narendra,",), "Ishika", "Pooja", ("Rohan",), "Sneha", ("Krishna",)]
+names = [('Darshit','Ashit','Devang'),'Ragi','Aashna','Darshini',('Darshil',)]
 count(names)
