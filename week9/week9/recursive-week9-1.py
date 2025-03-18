3cde6d2381251df105ce452935f5c960b9629f1a
Yash <126963834+yashharwani18@users.noreply.github.com>
2025-03-18 14:25:37 +0530

    Update recursive-week9-1.py

--- week9/week9/recursive-week9-1.py
+++ week9/week9/recursive-week9-1.py
@@ -2,7 +2,7 @@ def prime_factors(n,i=2):
     if n ==1:
         return [] 
     if n % i ==0:
-        return [i]+prime_factors(n/i,i+1)
+        return [i]+prime_factors(n/i,i)
     else:
         return prime_factors(n,i+1)
 num = int(input("Enter a positive integer: "))
